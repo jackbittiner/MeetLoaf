8edf02475ba5a3ef1e10e8e5d2692e290467260f
Jack Bittiner <jackbittiner@hotmail.com>
2018-04-26 21:27:24 +0100

    :fire: removed css files and some create react app stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
 import Sprint from "./components/sprint";
 import Timer from "./components/timer";
 
