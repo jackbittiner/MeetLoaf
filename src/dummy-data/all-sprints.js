030ceb6fae2edd9fc78d9d744dbc0cca497109d0
Jack Bittiner <jackbittiner@hotmail.com>
2018-04-30 13:59:39 +0100

    TML25 :construction: Added dummy data for past sprints
    to be used for future implementation

---
+++ src/dummy-data/all-sprints.js
@@ -0,0 +1,185 @@
+export const allSprints = {
+
+  sprints: [
+
+    {
+      id: 1,
+      meetings: [
+        {
+          id: 1,
+          numberOfAttendees: 4,
+          meetingLength: 3600
+        },
+        {
+          id: 2,
+          numberOfAttendees: 3,
+          meetingLength: 3000
+        },
+        {
+          id: 3,
+          numberOfAttendees: 5,
+          meetingLength: 1800
+        },
+        {
+          id: 4,
+          numberOfAttendees: 5,
+          meetingLength: 1000
+        },
+        {
+          id: 5,
+          numberOfAttendees: 4,
+          meetingLength: 3500
+        },
+        {
+          id: 6,
+          numberOfAttendees: 5,
+          meetingLength: 3333
+        }
+      ]
+    },
+
+    {
+      id: 2,
+      meetings: [
+        {
+          id: 7,
+          numberOfAttendees: 4,
+          meetingLength: 3600
+        },
+        {
+          id: 8,
+          numberOfAttendees: 3,
+          meetingLength: 3000
+        },
+        {
+          id: 9,
+          numberOfAttendees: 5,
+          meetingLength: 1800
+        },
+        {
+          id: 10,
+          numberOfAttendees: 5,
+          meetingLength: 1000
+        },
+        {
+          id: 11,
+          numberOfAttendees: 4,
+          meetingLength: 3500
+        },
+        {
+          id: 12,
+          numberOfAttendees: 5,
+          meetingLength: 3333
+        }
+      ]
+    },
+
+    {
+      id: 3,
+      meetings: [
+        {
+          id: 13,
+          numberOfAttendees: 4,
+          meetingLength: 3600
+        },
+        {
+          id: 14,
+          numberOfAttendees: 3,
+          meetingLength: 3000
+        },
+        {
+          id: 15,
+          numberOfAttendees: 5,
+          meetingLength: 1800
+        },
+        {
+          id: 16,
+          numberOfAttendees: 5,
+          meetingLength: 1000
+        },
+        {
+          id: 17,
+          numberOfAttendees: 4,
+          meetingLength: 3500
+        },
+        {
+          id: 18,
+          numberOfAttendees: 5,
+          meetingLength: 3333
+        }
+      ]
+    },
+
+    {
+      id: 4,
+      meetings: [
+        {
+          id: 19,
+          numberOfAttendees: 4,
+          meetingLength: 3600
+        },
+        {
+          id: 20,
+          numberOfAttendees: 3,
+          meetingLength: 3000
+        },
+        {
+          id: 21,
+          numberOfAttendees: 5,
+          meetingLength: 1800
+        },
+        {
+          id: 22,
+          numberOfAttendees: 5,
+          meetingLength: 1000
+        },
+        {
+          id: 23,
+          numberOfAttendees: 4,
+          meetingLength: 3500
+        },
+        {
+          id: 24,
+          numberOfAttendees: 5,
+          meetingLength: 3333
+        }
+      ]
+    },
+
+    {
+      id: 5,
+      meetings: [
+        {
+          id: 25,
+          numberOfAttendees: 4,
+          meetingLength: 3600
+        },
+        {
+          id: 26,
+          numberOfAttendees: 3,
+          meetingLength: 3000
+        },
+        {
+          id: 27,
+          numberOfAttendees: 5,
+          meetingLength: 1800
+        },
+        {
+          id: 28,
+          numberOfAttendees: 5,
+          meetingLength: 1000
+        },
+        {
+          id: 29,
+          numberOfAttendees: 4,
+          meetingLength: 3500
+        },
+        {
+          id: 30,
+          numberOfAttendees: 5,
+          meetingLength: 3333
+        }
+      ]
+    }
+  ]
+};
